a6aad6b6565038767d5ef2d50e8195e3f4090313
Ferid Ehmedli <feridehmedli@Farids-MacBook-Air.local>
2024-02-22 22:04:38 +0400

    uptade

--- etsy/src/components/site/basketPage/BasketPage.jsx
+++ etsy/src/components/site/basketPage/BasketPage.jsx
@@ -21,55 +21,59 @@ const BasketPage = () => {
 
   return (
     <div className="basket">
-      <div className="basket__items">
-        <div className="total__price">
-          <span>Total Price : {calculateTotalPrice()} $</span>
-        </div>
-        <div className="basket__card">
-          <div className="card__itemss">
-            {basketItems.map((item, index) => (
-              <div key={index} className="card__item">
-                <div className="card__images">
-                  <img src={item.item.image} alt="" />
-                </div>
-                <div className="card__titles">
-                  <span>{item.item.name?.slice(0, 45)}</span>
-                </div>
-                <div className="button__class">
-                  <div className="decrease">
-                    <button
-                      disabled={item.count == 1}
-                      onClick={() => decreaseFromBasket(item)}
-                    >
-                      -
-                    </button>
+      {basketItems.length === 0 ? (
+        <div className="empty-basket-message" style={{"fontWeight" : "lighter","fontSize":"30px"}}>Basket is empty</div>
+      ) : (
+        <div className="basket__items">
+          <div className="total__price">
+            <span>Total Price : {calculateTotalPrice()} $</span>
+          </div>
+          <div className="basket__card">
+            <div className="card__itemss">
+              {basketItems.map((item, index) => (
+                <div key={index} className="card__item">
+                  <div className="card__images">
+                    <img src={item.item.image} alt="" />
                   </div>
-                  <div className="count">
-                    <span style={{ fontWeight: "bold" }}>{item.count}</span>
+                  <div className="card__titles">
+                    <span>{item.item.name?.slice(0, 45)}</span>
                   </div>
-                  <div className="increase">
-                    <button onClick={() => handleAddToBasket(item)}>+</button>
+                  <div className="button__class">
+                    <div className="decrease">
+                      <button
+                        disabled={item.count === 1}
+                        onClick={() => decreaseFromBasket(item)}
+                      >
+                        -
+                      </button>
+                    </div>
+                    <div className="count">
+                      <span style={{ fontWeight: "bold" }}>{item.count}</span>
+                    </div>
+                    <div className="increase">
+                      <button onClick={() => handleAddToBasket(item)}>+</button>
+                    </div>
+                  </div>
+                  <div className="price__class">
+                    <span style={{ fontWeight: "lighter" }}>Price :</span>{" "}
+                    <span style={{ fontWeight: "bold" }}>
+                      {" "}
+                      {item.item.price} $
+                    </span>
+                  </div>
+                  <div className="delete__basket" onClick={() => { toast.error("Product was deleted from basket") }}>
+                    <RiDeleteBinFill
+                      onClick={() => {
+                        removeFromBasket(item);
+                      }}
+                    />
                   </div>
                 </div>
-                <div className="price__class">
-                  <span style={{ fontWeight: "lighter" }}>Price :</span>{" "}
-                  <span style={{ fontWeight: "bold" }}>
-                    {" "}
-                    {item.item.price} $
-                  </span>
-                </div>
-                <div className="delete__basket" onClick={()=>{toast.error("Product was deleted from basket")}}>
-                  <RiDeleteBinFill
-                    onClick={() => {
-                      removeFromBasket(item);
-                    }}
-                  />
-                </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
--- etsy/src/components/site/jewelleryCategories/Jewellery.jsx
+++ etsy/src/components/site/jewelleryCategories/Jewellery.jsx
@@ -4,25 +4,22 @@ import { IoIosStar } from "react-icons/io";
 import { FaRegHeart } from "react-icons/fa";
 import axios from "axios";
 import { MainContext } from "../../../context/Context";
-import { Link } from "react-router-dom";
 import toast from "react-hot-toast";
 
 const Jewellery = () => {
   const [data, setData] = useState([]);
   const [sortOption, setSortOption] = useState("relevancy");
-  const { handleAddWishlist,handleAddToBasket } = useContext(MainContext);
-  
+  const { handleAddToBasket } = useContext(MainContext);
+  const { handleAddWishlist } = useContext(MainContext);
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("http://localhost:8080/jewellery");
-        setData(response.data);
-      } catch (error) {
+    axios
+      .get("http://localhost:8080/jewellery")
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((error) => {
         console.error("Error fetching data:", error);
-      }
-    };
-
-    fetchData();
+      });
   }, []);
 
   const handleSortChange = (e) => {
@@ -43,96 +40,100 @@ const Jewellery = () => {
         <div className="accesories__items">
           <div className="section__ones">
             <div className="category__title">
-              <span>Jewellery</span>
+              <span>Jewelry</span>
               <span className="smalls">
-                Rings, earrings, and necklaces to instantly raise your
-                jewelry-box game
+              Rings, earrings and necklaces to instantly raise your jewelry-box game
               </span>
             </div>
             <div className="small__category">
               <div className="section">
                 <img
-                  src="https://i.etsystatic.com/8595828/c/3000/3000/0/0/il/2aa7b1/2294321673/il_300x300.2294321673_5ybn.jpg"
-                  alt="Rings"
-                />
-                <span>Rings</span>
-              </div>
-              <div className="section">
-                <img
                   src="https://i.etsystatic.com/7770785/r/il/d55a82/4328838368/il_300x300.4328838368_iyyu.jpg"
-                  alt="Necklaces"
+                  alt=""
                 />
                 <span>Necklaces</span>
               </div>
               <div className="section">
                 <img
+                  src="https://i.etsystatic.com/8595828/c/3000/3000/0/0/il/2aa7b1/2294321673/il_300x300.2294321673_5ybn.jpg"
+                  alt=""
+                />
+                <span>Rings</span>
+              </div>
+              <div className="section">
+                <img
                   src="https://i.etsystatic.com/17246174/r/il/2cf410/2546323062/il_300x300.2546323062_d78f.jpg"
-                  alt="Earrings"
+                  alt=""
                 />
                 <span>Earrings</span>
               </div>
               <div className="section">
                 <img
-                  src="https://i.etsystatic.com/5204269/c/1100/1100/0/0/il/88e8eb/1053449136/il_300x300.1053449136_ngza.jpg"
-                  alt="Bracelets"
+                  src="https://i.etsystatic.com/5152054/r/il/61e16f/1818566504/il_300x300.1818566504_ohit.jpg"
+                  alt=""
                 />
                 <span>Bracelets</span>
               </div>
               <div className="section">
                 <img
                   src="https://i.etsystatic.com/7704307/r/il/ed36fc/2341970317/il_300x300.2341970317_gqku.jpg"
-                  alt="Watches"
+                  alt=""
                 />
                 <span>Watches</span>
               </div>
               <div className="section">
                 <img
                   src="https://i.etsystatic.com/10204022/c/2586/2586/320/142/il/09c121/2731910039/il_300x300.2731910039_d8wd.jpg"
-                  alt="Cremation & Memorial Jewelry"
+                  alt=""
                 />
                 <span>Cremation & Memorial Jewelry</span>
               </div>
             </div>
           </div>
-          <div className="products">
-            <div className="sort">
-              <select
-                name="filter"
-                id="filter"
-                onChange={handleSortChange}
-                value={sortOption}
-              >
-                <option value="relevancy">Relevancy</option>
-                <option value="lowtohigh">From cheap to expensive</option>
-                <option value="hightolow">From expensive to cheap</option>
-              </select>
-            </div>
-            <div className="product__card">
-              {sortedData.map((item, index) => (
-                <div className="product__card__items" key={index}>
-                  <div className="product__card__image">
-                    <Link to={`/${item._id}`}>
-                      <img src={item.image} alt={item.name} />
-                    </Link>
-                    <div className="favourites">
-                      <FaRegHeart onClick={() => handleAddWishlist(item)} />
-                    </div>
+        </div>
+        <div className="products">
+          <div className="sort">
+            <select
+              name="filter"
+              id="filter"
+              onChange={handleSortChange}
+              value={sortOption}
+            >
+              <option value="relevancy">Relevancy</option>
+              <option value="lowtohigh">From cheap to expensive</option>
+              <option value="hightolow">From expensive to cheap</option>
+            </select>
+          </div>
+          <div className="product__card">
+            {sortedData.map((item, index) => (
+              <div className="product__card__items" key={index}>
+                <div className="product__card__image">
+                  <img src={item.image} alt="" />
+                  <div className="favourites">
+                    <FaRegHeart
+                      onClick={() => {
+                        handleAddWishlist(item);
+                      }}
+                    />
                   </div>
-                  <div className="card__down">
-                    <div className="product__card__title">
-                      <span>{item.name.slice(0, 45)}</span>
-                    </div>
-                    <div className="stars">
-                      {[...Array(5)].map((_, i) => (
-                        <IoIosStar key={i} />
-                      ))}
-                    </div>
-                    <div className="product__prices">
-                      <span>USD {item.price}</span>
-                    </div>
-                    <div className="ad__by">
-                      <span>Ad by {item.adBy}</span>
-                      <div className="addToCartButton">
+                </div>
+                <div className="card__down">
+                  <div className="product__card__title">
+                    <span>{item.name.slice(0, 45)}</span>
+                  </div>
+                  <div className="stars">
+                    <IoIosStar />
+                    <IoIosStar />
+                    <IoIosStar />
+                    <IoIosStar />
+                    <IoIosStar />
+                  </div>
+                  <div className="product__prices">
+                    <span>USD {item.price}</span>
+                  </div>
+                  <div className="ad__by">
+                    <span>Ad by {item.adBy}</span>
+                    <div className="addToCartButton">
                       <button
                         onClick={() => {
                           handleAddToBasket(item);
@@ -142,11 +143,10 @@ const Jewellery = () => {
                         Add to cart
                       </button>
                     </div>
-                    </div>
                   </div>
                 </div>
-              ))}
-            </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
